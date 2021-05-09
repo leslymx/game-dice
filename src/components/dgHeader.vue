7b78f9717bb8834c592bec2c1109ada313d7f8a1
lesly <lkpsmx@gmail.com>
2021-05-08 19:47:17 -0500

    dice-game finished

---
+++ src/components/dgHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav>
+      <img src="../assets/icon.png" alt="Dados" class="nav__img" />
+      <h1>DICE GAME</h1>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: "dg-header",
+};
+</script>
+
+
+<style scoped>
+header {
+  height: 100px;
+  width: 100vw;
+}
+
+.nav__img {
+  height: 70px;
+  width: 70px;
+}
+</style>
